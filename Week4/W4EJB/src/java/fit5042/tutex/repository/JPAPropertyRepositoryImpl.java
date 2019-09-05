9468f96cb619e0ce7513b665ecd4d0a3f4438f86
AkhileshSoorneedi <48817190+AkhileshSoorneedi@users.noreply.github.com>
2019-09-05 19:41:22 +1000

    Weekly tasks

---
+++ Week4/W4EJB/src/java/fit5042/tutex/repository/JPAPropertyRepositoryImpl.java
@@ -0,0 +1,90 @@
+package fit5042.tutex.repository;
+
+import fit5042.tutex.repository.entities.ContactPerson;
+import fit5042.tutex.repository.entities.Property;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author Eddie Leung
+ */
+@Stateless
+public class JPAPropertyRepositoryImpl implements PropertyRepository {
+
+    @PersistenceContext(name = "W4EJBPU")//insert code (annotation) here to use container managed entity manager to complete these methods  
+    private EntityManager entityManager;
+
+    @Override
+    public void addProperty(Property property) throws Exception {
+        List<Property> properties = entityManager.createNamedQuery(Property.GET_ALL_QUERY_NAME).getResultList();
+        property.setPropertyId(properties.get(0).getPropertyId() + 1);
+        entityManager.persist(property);
+    }
+
+    @Override
+    public Property searchPropertyById(int id) throws Exception {
+        Property property = entityManager.find(Property.class, id);
+        property.getTags();
+        return property;
+    }
+
+    @Override
+    public List<Property> getAllProperties() throws Exception {
+        return entityManager.createNamedQuery(Property.GET_ALL_QUERY_NAME).getResultList();
+    }
+
+    @Override
+    public Set<Property> searchPropertyByContactPerson(ContactPerson contactPerson) throws Exception {
+        contactPerson = entityManager.find(ContactPerson.class, contactPerson.getConactPersonId());
+        contactPerson.getProperties().size();
+        entityManager.refresh(contactPerson);
+
+        return contactPerson.getProperties();
+    }
+
+    @Override
+    public List<ContactPerson> getAllContactPeople() throws Exception {
+        return entityManager.createNamedQuery(ContactPerson.GET_ALL_QUERY_NAME).getResultList();
+    }
+
+    @Override
+    public void removeProperty(int propertyId) throws Exception {
+        Property property = this.searchPropertyById(propertyId);
+        if(property!=null)
+        {
+            entityManager.remove(property);
+        }
+    }
+
+    @Override
+    public void editProperty(Property property) throws Exception {
+        try {
+            entityManager.merge(property);
+        } catch (Exception ex) {
+
+        }
+    }
+
+    @Override
+    public List<Property> searchPropertyByBudget(double budget) throws Exception {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Property> q = cb.createQuery(Property.class);
+        Root<Property> r = q.from(Property.class);
+        Predicate condition = cb.le(r.get("price").as(Double.class), budget);
+        q.where(condition);
+        TypedQuery<Property> tq = entityManager.createQuery(q);
+        List<Property> result = tq.getResultList();
+        return result;
+    }
+}
